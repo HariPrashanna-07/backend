ce5e7f643c23534d7a8bd9b75acdd4443c4414ea
HariPrashannaV <hariprashanna.v2024eee@sece.ac.in>
2025-12-18 12:17:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,21 @@ const app = express();
 // ✅ Render provides its own PORT
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+// Middlewareconst cors = require("cors");
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://frontend-eight-teal-30.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+// IMPORTANT: handle preflight
+app.options("*", cors());
+
 app.use(express.json());
 
 // Routes
